93792057fb2cc341ff918c258ceccfc313e311c6
noorilyas <noorilyas907@gmail.com>
2025-07-04 22:23:06 +0500

    feat: added assignment-1 base route and layout

---
+++ src/app/assignment-1/page.js
@@ -0,0 +1,7 @@
+export default function QuotePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-10">
+      <h1 className="text-3xl font-bold">Assignment 1: Quote Generator</h1>
+    </div>
+  );
+}
