961c1c55ea9149e3c44ce93e3271634f1eaf9a24
noorilyas <noorilyas907@gmail.com>
2025-07-06 11:46:12 +0500

    feat: initialize ShadCN UI with Zinc theme

--- src/app/assignment-1/page.js
+++
@@ -1,7 +0,0 @@
-export default function QuotePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center p-10">
-      <h1 className="text-3xl font-bold">Assignment 1: Quote Generator</h1>
-    </div>
-  );
-}
